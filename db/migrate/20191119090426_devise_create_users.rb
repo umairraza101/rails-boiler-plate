e6759419b5c31a1f1cc6a8b979a1b054322bcb1f
Muhammad umair Raza <umair.raza101@gmail.com>
2019-11-19 15:51:16 +0500

    setup application

---
+++ db/migrate/20191119090426_devise_create_users.rb
@@ -0,0 +1,20 @@
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, default: ''
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.string :encrypted_password, null: false, default: ''
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+      t.string :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.timestamps null: false
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :reset_password_token, unique: true
+  end
+end
